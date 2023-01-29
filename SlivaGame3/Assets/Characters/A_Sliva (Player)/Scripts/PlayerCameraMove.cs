0748fb337f3eb994bda77994663138fc0d604d74
Richa77777 <121635433+Richa77777@users.noreply.github.com>
2023-01-29 22:52:57 +0300

    Step System Update

--- SlivaGame3/Assets/Characters/A_Sliva (Player)/Scripts/PlayerCameraMove.cs
+++ SlivaGame3/Assets/Characters/A_Sliva (Player)/Scripts/PlayerCameraMove.cs
@@ -9,8 +9,6 @@ namespace Player
     {
         private Camera _camera;
 
-        [SerializeField] private GameObject _target;
-
         [Range(0.0f, 10.0f)]
         [SerializeField] private float _cameraSpeed;
 
@@ -18,7 +16,7 @@ namespace Player
         private void Start()
         {
             _camera = Camera.main;
-            _camera.transform.localPosition = new Vector3(_target.transform.localPosition.x, _target.transform.localPosition.y, -10);
+            _camera.transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, -10);
         }
 
         private void Update()
@@ -29,7 +27,7 @@ namespace Player
 
         private void CameraMove()
         {
-            Vector3 targetPos = new Vector3(_target.transform.localPosition.x, _target.transform.localPosition.y, -10);
+            Vector3 targetPos = new Vector3(transform.localPosition.x, transform.localPosition.y, -10);
             _camera.transform.localPosition = Vector3.Lerp(_camera.transform.localPosition, targetPos, _cameraSpeed * Time.deltaTime);
         }
     }
--- SlivaGame3/Assets/Characters/A_Sliva (Player)/Scripts/StepSoundPlay.cs
+++ SlivaGame3/Assets/Characters/A_Sliva (Player)/Scripts/StepSoundPlay.cs
@@ -11,61 +11,61 @@ namespace Player.Sounds
         private struct StepSounds
         {
             [Serializable] 
-            private struct Walk
+            public struct Walk
             {
-                [SerializeField] private AudioClip _dirtyGround;
-                [SerializeField] private AudioClip _grass;
-                [SerializeField] private AudioClip _gravel;
-                [SerializeField] private AudioClip _leaves;
-                [SerializeField] private AudioClip _metal;
-                [SerializeField] private AudioClip _mud;
-                [SerializeField] private AudioClip _rock;
-                [SerializeField] private AudioClip _sand;
-                [SerializeField] private AudioClip _snow;
-                [SerializeField] private AudioClip _tile;
-                [SerializeField] private AudioClip _water;
-                [SerializeField] private AudioClip _wood;
-            }
-
-            [Serializable] 
-            private struct Run
-            {
-                [SerializeField] private AudioClip _dirtyGround;
-                [SerializeField] private AudioClip _grass;
-                [SerializeField] private AudioClip _gravel;
-                [SerializeField] private AudioClip _leaves;
-                [SerializeField] private AudioClip _metal;
-                [SerializeField] private AudioClip _mud;
-                [SerializeField] private AudioClip _rock;
-                [SerializeField] private AudioClip _sand;
-                [SerializeField] private AudioClip _snow;
-                [SerializeField] private AudioClip _tile;
-                [SerializeField] private AudioClip _water;
-                [SerializeField] private AudioClip _wood;
+                [SerializeField] public AudioClip _dirtyGround;
+                [SerializeField] public AudioClip _grass;
+                [SerializeField] public AudioClip _gravel;
+                [SerializeField] public AudioClip _leaves;
+                [SerializeField] public AudioClip _metal;
+                [SerializeField] public AudioClip _mud;
+                [SerializeField] public AudioClip _rock;
+                [SerializeField] public AudioClip _sand;
+                [SerializeField] public AudioClip _snow;
+                [SerializeField] public AudioClip _tile;
+                [SerializeField] public AudioClip _water;
+                [SerializeField] public AudioClip _wood;
             }
 
             [Serializable]
-            private struct Jump
+            public struct Run
             {
-                [SerializeField] private AudioClip _dirtyGround;
-                [SerializeField] private AudioClip _grass;
-                [SerializeField] private AudioClip _gravel;
-                [SerializeField] private AudioClip _leaves;
-                [SerializeField] private AudioClip _metal;
-                [SerializeField] private AudioClip _mud;
-                [SerializeField] private AudioClip _rock;
-                [SerializeField] private AudioClip _sand;
-                [SerializeField] private AudioClip _snow;
-                [SerializeField] private AudioClip _tile;
-                [SerializeField] private AudioClip _water;
-                [SerializeField] private AudioClip _wood;
+                [SerializeField] public AudioClip _dirtyGround;
+                [SerializeField] public AudioClip _grass;
+                [SerializeField] public AudioClip _gravel;
+                [SerializeField] public AudioClip _leaves;
+                [SerializeField] public AudioClip _metal;
+                [SerializeField] public AudioClip _mud;
+                [SerializeField] public AudioClip _rock;
+                [SerializeField] public AudioClip _sand;
+                [SerializeField] public AudioClip _snow;
+                [SerializeField] public AudioClip _tile;
+                [SerializeField] public AudioClip _water;
+                [SerializeField] public AudioClip _wood;
             }
 
-            [SerializeField] private Walk _walk;
+            [Serializable]
+            public struct Jump
+            {
+                [SerializeField] public AudioClip _dirtyGround;
+                [SerializeField] public AudioClip _grass;
+                [SerializeField] public AudioClip _gravel;
+                [SerializeField] public AudioClip _leaves;
+                [SerializeField] public AudioClip _metal;
+                [SerializeField] public AudioClip _mud;
+                [SerializeField] public AudioClip _rock;
+                [SerializeField] public AudioClip _sand;
+                [SerializeField] public AudioClip _snow;
+                [SerializeField] public AudioClip _tile;
+                [SerializeField] public AudioClip _water;
+                [SerializeField] public AudioClip _wood;
+            }
+
+            [SerializeField] public Walk _walk;
             [Space(5)]
-            [SerializeField] private Run _run;
+            [SerializeField] public Run _run;
             [Space(5)]
-            [SerializeField] private Jump _jump;
+            [SerializeField] public Jump _jump;
         }
 
         [SerializeField] private StepSounds _sounds;
@@ -74,7 +74,7 @@ namespace Player.Sounds
         
         [SerializeField] private AudioClip _currentSound;
 
-        //public AudioClip CurrentSound { get { return _currentSound; } set { _currentSound = value; } }
+        public AudioClip CurrentSound { get { return _currentSound; } set { _currentSound = value; } }
 
         private AudioSource _audioSource;
 
@@ -86,6 +86,52 @@ namespace Player.Sounds
         public void PlaySound()
         {
             _audioSource?.PlayOneShot(_currentSound);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            string tag = collision.gameObject.tag;
+
+            switch (tag)
+            {
+                case "Dirty Ground":
+                    _currentSound = _sounds._walk._dirtyGround;
+                    break;
+                case "Grass":
+                    _currentSound = _sounds._walk._grass;
+                    break;
+                case "Gravel":
+                    _currentSound = _sounds._walk._gravel;
+                    break;
+                case "Leaves":
+                    _currentSound = _sounds._walk._leaves;
+                    break;
+                case "Metal":
+                    _currentSound = _sounds._walk._metal;
+                    break;
+                case "Mud":
+                    _currentSound = _sounds._walk._mud;
+                    break;
+                case "Rock":
+                    _currentSound = _sounds._walk._rock;
+                    break;
+                case "Sand":
+                    _currentSound = _sounds._walk._sand;
+                    break;
+                case "Snow":
+                    _currentSound = _sounds._walk._snow;
+                    break;
+                case "Tile":
+                    _currentSound = _sounds._walk._tile;
+                    break;
+                case "Water":
+                    _currentSound = _sounds._walk._water;
+                    break;
+                case "Wood":
+                    _currentSound = _sounds._walk._wood;
+                    break;
+
+            }
         }
     }
 }
